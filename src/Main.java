af3bd10669efda44b85d074d43a4f976f52bfe6a
aleks <abtrustno1@gmail.com>
2023-06-22 16:48:18 +0545

    Reformated code

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,17 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-    int amount = 1236;
-    int cashIn = 56;
+        int amount = 1236;
+        int cashIn = 56;
 
-    int totalAmount = amount + cashIn;
-    int bonus = 0;
-    if (cashIn > 1000)
-    {
-        bonus = cashIn / 100;
-        totalAmount = totalAmount + bonus;
-    }
-    System.out.println("Initial amount : " + amount + "; client added: " + cashIn);
-    System.out.println("Total amount: " + totalAmount + "; bonus: " + bonus);
+        int totalAmount = amount + cashIn;
+        int bonus = 0;
+        if (cashIn > 1000) {
+            bonus = cashIn / 100;
+            totalAmount = totalAmount + bonus;
+        }
+        System.out.println("Initial amount : " + amount + "; client added: " + cashIn);
+        System.out.println("Total amount: " + totalAmount + "; bonus: " + bonus);
 
 
     }
